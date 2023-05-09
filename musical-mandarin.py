55cf1ef0f46e9286b33c903955d8f02eaa40632a
N V <52893991+grassmanian@users.noreply.github.com>
2023-05-09 11:34:43 -0400

    Add slightly more informative error handling to script

--- musical-mandarin.py
+++ musical-mandarin.py
@@ -1,9 +1,19 @@
 import requests
+from requests import HTTPError
 from pydub import AudioSegment
 from pydub.playback import play
 import io
 
-response = requests.get('https://translate.google.com/translate_tts?ie=UTF-8&tl=zh_CN&client=tw-ob&q=話說天下大勢，分久必合，合久必分')
+try:
+    response = requests.get('https://translate.google.com/translate_tts?ie=UTF-8&tl=zh_CN&client=tw-ob&q=話說天下大勢，分久必合，合久必分')
+except:
+    print('Error connecting to Google Translate')
+else:
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        print('HTTP request returned status {}'.format(response))
+
 
 song = AudioSegment.from_file(io.BytesIO(response.content), format="mp3")
 play(song)
